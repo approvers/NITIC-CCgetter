bb7d6000baa215f4862fdef5fb7b7fc7d422bc6e
isso0424 <isso04240129@gmail.com>
2020-03-13 23:46:51 +0900

    intial commit

---
+++ app/src/main/java/com/example/nitic_cc_getter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nitic_cc_getter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
